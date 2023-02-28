d97ea585b1ec6fa758baeb7b77058ab343ad5914
Brett Huffman <dicer2000@yahoo.com>
2023-02-27 22:51:18 -0600

    Initial Update

---
+++ main.py
@@ -0,0 +1,51 @@
+
+import pygame as pg
+import sys
+from settings import *
+
+
+# Main game object
+class Game:
+    def __init__(self) -> None:
+        pg.init()
+        self.screen = pg.display.set_mode(RES)
+        self.clock = pg.time.Clock()
+        self.delta_time = 1
+        self.new_game()
+
+    def new_game(self):
+        # Create a new game
+        pass
+
+
+    def update(self):
+        self.delta_time = self.clock.tick(FPS)
+        pg.display.set_caption(f'Your Name - {self.clock.get_fps():.1f}')
+
+    def draw(self):
+        # Draw to the main Screen
+        self.screen.fill('black')
+        # Drawn screen to forefront
+        pg.display.update()
+
+    def check_events(self):
+        # Check for keyboard events
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+
+    def run(self):
+        # Called once to manage whole game
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+
+
+def main():
+    game = Game()
+    game.run()
+
+if __name__ == '__main__':
+    main()
---
+++ settings.py
@@ -0,0 +1,5 @@
+import pygame as pg
+
+# Window settings
+RES = WIDTH, HEIGHT = 1024, 1024
+FPS = 0
